1314e11871ecdcdd69ef1d4c9931b3c98af6df22
William Cheung <william.cheung@pb.com>
2024-06-16 08:11:47 -0400

    Convert from ChatInterface to Blocks. Add a "Get Nutrients from USDA" button and make the input section a panel with styling.

--- gradio_ui.py
+++ gradio_ui.py
@@ -12,7 +12,7 @@ llm = ChatOpenAI(temperature=1.0, model=os.getenv('OPENAI_MODEL'))
 def ask_ai(message: str, history: list[tuple[str, str]]) -> str:
     history_langchain_format: list[HumanMessage|AIMessage] = [
         SystemMessage(
-            content="You are a friendly and creative chef. But you only answer questions about recipes, nothing else!"
+            content='You are a friendly and creative chef. But you only answer questions about recipes, nothing else!'
         )
     ]
 
@@ -27,16 +27,56 @@ def ask_ai(message: str, history: list[tuple[str, str]]) -> str:
     gpt_response = llm.invoke(history_langchain_format)
     return gpt_response.content
 
-interface = gr.ChatInterface(ask_ai,
-                 title='Ingenium Nutrium',
-                 chatbot=gr.Chatbot(
-                     height=750,
-                     value=[(None,
-"""
+def submit_message(message: str, history: list[tuple[str, str]]) -> tuple[list[tuple[str, str]], str]:
+    response: str = ask_ai(message, history)
+    history.append((message, response))
+    return history, '' # '' clears the input text box
+
+def retry_message(history: list[tuple[str, str]]) -> tuple[list[tuple[str, str]], str]:
+    if history:
+        last_message: str = history[-1][0]
+        return submit_message(last_message, history[:-1])
+    return history, ''
+
+def undo_message(history: list[tuple[str, str]]) -> tuple[list[tuple[str, str]], str]:
+    if history:
+        return history[:-1], ''
+    return history, ''
+
+def clear_messages() -> tuple[list[tuple[str, str]], str]:
+    return [], ''
+
+with gr.Blocks() as demo:
+    gr.Markdown('<h1 style="text-align:center;">Ingenium Nutrium</h1>')
+
+    chatbot = gr.Chatbot(
+        label='Chatbot Chef',
+        height='60vh',
+        show_copy_button=True,
+        value=[(None,
+'''
 Hi, give me a list of ingredients for a meal you'd like to make...
 and I'll whip up a recipe for you!
 plus I'll even give you the nutritional value of your meal :)
-"""
-                    )])
-                 )
-interface.launch(server_name='0.0.0.0')
+'''
+        )]
+    )
+
+    with gr.Row(variant='panel'):
+        with gr.Column(scale=6):
+            msg = gr.Textbox(autofocus=True, label='Your ingredients', lines=2)
+        with gr.Column(scale=2):
+            send_button = gr.Button('Get Recipe')
+            nutrients_button = gr.Button('Get Nutrients from USDA')
+
+    with gr.Row():
+        retry_button = gr.Button('Retry')
+        undo_button = gr.Button('Undo')
+        clear_button = gr.Button('Clear')
+
+    send_button.click(submit_message, inputs=[msg, chatbot], outputs=[chatbot, msg])
+    retry_button.click(retry_message, inputs=[chatbot], outputs=[chatbot, msg])
+    undo_button.click(undo_message, inputs=[chatbot], outputs=[chatbot, msg])
+    clear_button.click(clear_messages, outputs=[chatbot, msg])
+
+demo.launch(server_name='0.0.0.0')
